97caeb8e7f9732f2da8f2324fe4e0156cb2b45a1
Leon Leibmann <leon.leibmann@gmail.com>
2025-06-23 21:55:01 -0700

    bugfix

--- modules/skipattnmlp.py
+++ modules/skipattnmlp.py
@@ -67,7 +67,7 @@ class SkipAttentionMLP(nn.Module):
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='gelu')
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm1d):
