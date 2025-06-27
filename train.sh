9e023e549771aef06ebf7bbee2eb6b3bf51f8cc8
Leon Leibmann <leon.leibmann@gmail.com>
2025-06-26 19:01:58 -0700

    fix .sh

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py --base_model geo_liquid_clip --depth 12 --embed_dim 4096 --num_hidden_dims 16384 --heads 64
+python3 train_model_nohierarchy.py --base_model geo_liquid_clip --depth 12 --embed_dim 4096 --num_hidden_dims 16384 --heads 64
