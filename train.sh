e21564e2baedec9cecb321fb3d5e42d04c200353
Leon Leibmann <leon.leibmann@gmail.com>
2025-07-03 22:37:17 -0700

    change train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-python3 train_model_nohierarchy.py --base_model clip
+python train_model_nohierarchy.py \
+  --batch_size 42 \
+  --batch_size_test 24 \
+  --max_batches 100000 \
+  --save_every 1000 \
+  --base_model clip \
+  --clip_model_name openai/clip-vit-large-patch14 \
+  --depth 12 \
+  --embed_dim 2048 \
+  --num_hidden_dims 8192 \
+  --heads 32 \
