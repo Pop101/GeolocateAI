d8f04c19273787422ad610fd13a604b1771e01ca
Leon Leibmann <leon.leibmann@gmail.com>
2025-07-05 17:46:34 -0700

    fix?

--- train_model_nohierarchy.py
+++ train_model_nohierarchy.py
@@ -321,6 +321,7 @@ def main():
     if args.compile:
         torch._dynamo.config.optimize_ddp = False
         torch._dynamo.config.capture_scalar_outputs = True
+        torch._dynamo.config.suppress_errors = True
         model.compile(
             fullgraph = True,
             dynamic   = False,
